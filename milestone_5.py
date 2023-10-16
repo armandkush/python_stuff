32aedb3ad0543cc3c84ab499cedf64fc74ddf799
Armand Kushairi <armandkushairi@gmail.com>
2023-10-16 17:23:53 +0800

    creating milestone_5.py

--- milestone_5.py
+++ milestone_5.py
@@ -26,28 +26,43 @@ class Hangman:
         else:
             self.num_lives = self.num_lives - 1
             print("Sorry," + guess + " is not in the word. Try again.")
-            print("You have " + self.num_lives + " lives left.")
+            print("You have " + str(self.num_lives) + " lives left.")
+            
 
     def ask_for_input(self):
         guess = input("Please enter a single letter for your guess \n")
         while True:
             if len(guess) != 1 or guess.isalpha() == False:
                 print("Invalid letter. Please, enter a single alphabetical character.")
-                
-            elif guess in self.list_of_guesses:
+                break
+            elif  guess in self.list_of_guesses:
+                print(self.list_of_guesses)
                 print("You already tried that letter!")
-                
+                break
             else: 
                 self.check_guess(guess)
                 self.list_of_guesses.append(guess)
-                if self.num_letters != 0:
-                    continue
-                else:
-                    break
+                print(self.list_of_guesses)
+                break
 
+
+def play_game(word_list):
+    num_lives = 5
+    game = Hangman(word_list = word_list, num_lives = num_lives )
+    while True:
+        if game.num_lives == 0:
+            print("You lost!")
+            break
+        elif game.num_letters >= 0:
+            game.ask_for_input()
+        elif game.num_lives != 0 and game.num_letters <= 0:
+            print("Congratulations. You won the game!")
+            break
 
 
 word_list = ['Banana','Orange','Strawberry','Apple','Chicken']
+
+play_game(word_list)
 
 
 
